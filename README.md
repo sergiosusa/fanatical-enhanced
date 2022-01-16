06bc536988a6e045ef62127d5ba84e655d95fcfc
Sergio Susa <sergio@sergiosusa.com>
2022-01-16 23:24:22 +0100

    - Add js file with the script. - Complete README.md file. - Upload screenshot folder with some files.

---
+++ README.md
@@ -0,0 +1,42 @@
+# ✨ Fanatical Enhanced ✨
+
+This script enhanced the famous marketplace **[Fanatical](https://www.fanatical.com/)** adding:
+
+* Add extra options in order page: 
+  * Reveal keys with one click.
+  * Copy keys on excel format to the clipboard.
+  * Copy keys on a list separated by "," to the clipboard.
+
+### Supported sites
+
+- [Fanatical](https://www.fanatical.com/)
+
+## 📌Prerequisites📎
+
+- [Tampermonkey (Chrome)](https://tampermonkey.net)
+- [Greasemonkey (Firefox)](http://www.greasespot.net)
+- [Violent monkey (Opera)](https://addons.opera.com/sk/extensions/details/violent-monkey/)
+
+## 🖥️Installation🖱️
+
+**Github**
+
+- Enter to the user script file (usually named as <code>*.user.js</code>) you want to install.
+- Click on the <code>Raw</code> button, the browser extension will recognize this file as a user script.
+- Install it.
+
+That's it!
+
+## 🔧Configuration🔧
+
+No extra configuration required.
+
+## 📷Screenshots📷
+
+![Add extra options in order page](screenshots/fanatical-enhanced.webp)
+
+### ☕Buy me a coffee☕
+
+- My Amazon [affiliate link](https://amazon.es/?tag=sergiosusa-21) or add this query string ``?tag=sergiosusa-21`` before add a product to the basket when you buy on Amazon spain.
+- My Letyshops [referal link](https://letyshops.com/es/winwin?ww=17530599) to get €5 each.
+- My Payme [link](https://paypal.me/sergiosusa?locale.x=es_ES) to send me a tip.
---
+++ fanatical-enhanced.user.js
@@ -0,0 +1,115 @@
+// ==UserScript==
+// @name         Fanatical Enhanced
+// @namespace    https://sergiosusa.com/
+// @version      0.1
+// @description  This script enhanced the famous marketplace fanatical with some extra features.
+// @author       Sergio Susa (sergio@sergiosusa.com)
+// @match        https://www.fanatical.com/en/orders/*
+// @icon         https://www.google.com/s2/favicons?domain=fanatical.com
+// @grant        GM_setClipboard
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    try {
+        let fanaticalEnhanced = new FanaticalEnhanced();
+        fanaticalEnhanced.render();
+    } catch (exception) {
+        alert(exception);
+    }
+})();
+
+function FanaticalEnhanced() {
+
+    this.rendererList = [
+        new OrderRevealer()
+    ];
+
+    this.render = () => {
+        let renderer = this.findRenderer();
+        setTimeout( renderer.render,2000);
+    }
+
+    this.findRenderer = () => {
+        return this.rendererList.find(renderer => renderer.canHandleCurrentPage());
+    };
+
+}
+
+function Renderer() {
+    this.handlePage = "";
+
+    this.canHandleCurrentPage = () => {
+        return document.location.href.includes(this.handlePage);
+    };
+
+    this.showAlert = (text) => {
+        alert(text);
+    }
+}
+
+function OrderRevealer() {
+    Renderer.call(this);
+    this.handlePage = "https://www.fanatical.com/en/orders";
+
+    this.render = () => {
+        let referenceElement = document.querySelector('div.title-download-button-container');
+
+        let buttonContainer = document.createElement("div");
+        buttonContainer.className = 'title-button-container';
+        buttonContainer.innerHTML =
+            '<button type="button" id="reveal-keys-button" class="btn-line-account btn btn-secondary">Reveal Keys</button>' +
+            '<button type="button" id="copy-excel-button" class="btn-line-account btn btn-secondary">Copy Excel</button>' +
+            '<button type="button" id="copy-list-button" class="btn-line-account btn btn-secondary">Copy List</button>';
+
+        referenceElement.parentElement.insertBefore(buttonContainer, referenceElement.nextElementSibling);
+
+        document.querySelector("#reveal-keys-button").onclick = this.revealKeys;
+        document.querySelector("#copy-excel-button").onclick = this.copyExcel;
+        document.querySelector("#copy-list-button").onclick = this.copyList;
+
+    };
+
+    this.revealKeys = () => {
+        let revealButtons = document.querySelectorAll("div.key-container button");
+
+        revealButtons.forEach((element) => {
+            element.click();
+        });
+    };
+
+    this.copyExcel = () => {
+
+        let result = [];
+
+        document.querySelectorAll("div.order-item-details-container").forEach((gameElement) => {
+            let gameName = gameElement.querySelector("div.game-name").innerText;
+            let gameKey = gameElement.querySelector("input.key-input-field").value;
+
+            result.push(gameName + "\t" + gameKey)
+        });
+
+        this.copyToClipboard(result.join("\n"));
+    };
+
+    this.copyList = () => {
+        let result = [];
+
+        document.querySelectorAll("div.order-item-details-container").forEach((gameElement) => {
+            let gameKey = gameElement.querySelector("input.key-input-field").value;
+
+            result.push(gameKey)
+        });
+
+        this.copyToClipboard(result.join());
+    };
+
+    this.copyToClipboard = (list) => {
+        GM_setClipboard(list);
+        this.showAlert("Seriales copiados al portapapeles");
+    };
+
+}
+
+OrderRevealer.prototype = Object.create(Renderer.prototype);
